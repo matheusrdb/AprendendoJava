62c3594fb5f73b9801cea2dbe6c213470483ea06
Matheus Rubens <matheusrb8@outlook.com.br>
2021-05-25 10:21:29 -0300

    FQN, Nomenclatura de Pacote e configuração do eclipse

---
+++ EclipseJavaBibliotecas/java5-projetos-inicias.zip_expanded/bytebank-herdado-conta/src/br/com/bytebank/modelo/SaldoInsuficienteException.java
@@ -0,0 +1,9 @@
+package br.com.bytebank.modelo;
+
+public class SaldoInsuficienteException extends Exception{
+	
+	public SaldoInsuficienteException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ EclipseJavaBibliotecas/java5-projetos-inicias.zip_expanded/bytebank-herdado-conta/src/br/com/bytebank/modelo/Tributavel.java
@@ -0,0 +1,6 @@
+package br.com.bytebank.modelo;
+
+public interface Tributavel {
+
+	double getValorImposto();
+}
